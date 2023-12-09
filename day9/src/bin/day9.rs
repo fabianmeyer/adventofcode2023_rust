30bff96f0050fd42e0a48728aeca7327185fcfee
Fabian Meyer <fameyer@brita.net>
2023-12-09 13:08:45 +0100

    Renamed interpolate into extrapolate and used move instead of copying for reversing input

--- day9/src/bin/day9.rs
+++ day9/src/bin/day9.rs
@@ -5,27 +5,27 @@ fn main() {
     let result = read_to_string("day9/data/input.txt").map(parse_input);
     match result {
         Ok(input) => {
-            let extrapolated_next_sum = input.iter().map(interpolate).sum::<i64>();
+            let extrapolated_next_sum = input.iter().map(extrapolate).sum::<i64>();
             println!("{:?}", extrapolated_next_sum);
-            let reversed = input.iter().map(|v| v.iter().rev().map(|it| it.clone()).collect_vec()).collect_vec();
-            let extrapolated_prev_sum = reversed.iter().map(interpolate).sum::<i64>();
+            let reversed = input.into_iter().map(|v| v.into_iter().rev().collect_vec()).collect_vec();
+            let extrapolated_prev_sum = reversed.iter().map(extrapolate).sum::<i64>();
             println!("{:?}", extrapolated_prev_sum);
         }
         Err(err) => { println!("{:?}", err); }
     }
 }
 
-fn interpolate(values: &Vec<i64>) -> i64 {
-    let extrapolated = if values.iter().all(|v| *v == 0) {
+fn extrapolate(values: &Vec<i64>) -> i64 {
+    let extrapolated_next = if values.iter().all(|v| *v == 0) {
         0
     } else {
         let differences = values.iter().zip(values.iter().skip(1)).map(|(a, b)| b - a).collect_vec();
-        let difference = interpolate(&differences);
+        let extrapolated_next_difference = extrapolate(&differences);
         let last = values.last().unwrap();
-        last + difference
+        last + extrapolated_next_difference
     };
 
-    return extrapolated;
+    return extrapolated_next;
 }
 
 // General
